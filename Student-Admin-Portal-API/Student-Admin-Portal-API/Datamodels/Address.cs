a1cd902a60197c35a3c5bd4caff3ac2c025e9bec
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2022-11-26 15:43:34 +0600

    Datamodel and DbContex Added

---
+++ Student-Admin-Portal-API/Student-Admin-Portal-API/Datamodels/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Student_Admin_Portal_API.Datamodels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+        //Navigation Property
+        public Guid StudentId { get; set; }
+    }
+}
---
+++ Student-Admin-Portal-API/Student-Admin-Portal-API/Datamodels/Gender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Student_Admin_Portal_API.Datamodels
+{
+    public class Gender
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+    }
+}
